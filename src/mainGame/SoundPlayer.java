a0994b823457fe5e96681902765fa5e695198395
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2017-10-17 13:28:08 -0400

    bitch betta fix my paths

--- src/mainGame/SoundPlayer.java
+++ src/mainGame/SoundPlayer.java
@@ -19,34 +19,17 @@ public class SoundPlayer extends Thread {
 	public void run() {
 		try {
 			File f = new File(Game.class.getResource(soundfile).toURI().toString());
-			Media song = new Media(f.toString());
+			Media song = new Media(f.toString().replaceAll("\\\\", "/"));
 			player = new MediaPlayer(song);
 			player.setVolume(0.25);
 			player.play();
 		} catch (URISyntaxException use) {
 			use.printStackTrace();
 		}
-		/*
-		try {
-			File f = new File(Game.class.getResource(soundfile).toURI());
-			FileInputStream fileInputStream = new FileInputStream(f);
-			player = new Player(fileInputStream);
-			player.play();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (JavaLayerException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException use) {
-			use.printStackTrace();
-		}*/
 	}
 
 	public void play() {
-		//try {
-			player.play();
-		//} catch (JavaLayerException jle) {
-		//	jle.printStackTrace();
-		//}
+		player.play();
 	}
 
 	public void pause() {
