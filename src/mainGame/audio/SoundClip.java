c1966ab04f4c042e1eac4e282414e7d6645aa424
Will Eccles <willeccles860+github@gmail.com>
2017-10-20 15:01:47 -0400

    renamed Clip to SoundClip for clarity

--- src/mainGame/audio/SoundClip.java
+++ src/mainGame/audio/SoundClip.java
@@ -6,7 +6,12 @@ import java.awt.Toolkit;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-public class Clip {
+/**
+ * A sound clip that is played asynchronously.
+ * @author Will Eccles
+ * @version 2017-10-20
+ */
+public class SoundClip {
 	private double volume; // double from 0.0-1.0 representing the relative volume to play the file at. Will be clamped at playback, so even 123.123 just = 1.0
 	private Media media; // the media that will be played
 
@@ -15,7 +20,7 @@ public class Clip {
 	 * @param filesource The path to the audio clip to play.
 	 * @param volume The volume to play the file at (clamped to 0.0-1.0).
 	 */
-	public Clip(String filesource, double volume) {
+	public SoundClip(String filesource, double volume) {
 		this.volume = volume;
 		try {
 			File f = new File(Game.class.getResource(filesource).toURI().toString());
@@ -29,7 +34,7 @@ public class Clip {
 	 * Constructor.
 	 * @param filesource The path to the audio clip to play.
 	 */
-	public Clip(String filesource) {
+	public SoundClip(String filesource) {
 		this(filesource, 1.0);
 	}
 
