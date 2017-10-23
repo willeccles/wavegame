63b996c5afb993d6337220c7fccadb240f9f7a87
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2017-10-23 12:33:51 -0400

    Fixed survival

--- src/mainGame/SpawnSurvival.java
+++ src/mainGame/SpawnSurvival.java
@@ -118,6 +118,7 @@ public class SpawnSurvival {
 				spawnTimer = 0;
 			} else if (temp == 8){
 				handler.addObject(new EnemyPorcupine(r.nextInt(Game.WIDTH) - 35, r.nextInt(Game.HEIGHT) - 75, 100, 100, ID.EnemyPorcupine, this.handler, -1, -2));
+				spawnTimer = 0;
 			}
 		}
 		spawnTimer++;
