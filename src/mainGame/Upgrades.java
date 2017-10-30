3fa43e7b4800871839487f77f242e76a8c43d9e0
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2017-10-30 17:53:46 -0400

    Fixed so usage of power ups wont go below 0 stop giving me these one character fixes it is starting to get annoying

--- src/mainGame/Upgrades.java
+++ src/mainGame/Upgrades.java
@@ -37,7 +37,7 @@ public class Upgrades {
 	public void clearScreenAbility() {
 		handler.clearEnemies();
 		hud.setAbilityUses(hud.getAbilityUses() - 1);
-		if (hud.getAbilityUses() == 0) {
+		if (hud.getAbilityUses() >= 0) {
 			ability = "";
 		}
 	}
