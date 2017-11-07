f2ae2702c52be87fad6c6738bab7375221b594f3
Will Eccles <willeccles860+github@gmail.com>
2017-11-06 20:00:51 -0500

    removed spawning for now to make multi ok

--- src/server/Instance.java
+++ src/server/Instance.java
@@ -107,7 +107,7 @@ public class Instance extends Thread {
 		
 		// send to each of the clients
 		// msg: SPAWN:<ID ordinal>,x,y,option,side
-		sendToAll("SPAWN:" + e.getType().ordinal() + ',' + e.getX() + ',' + e.getY() + ',' + e.getOption() + ',' + e.getSide());
+		//sendToAll("SPAWN:" + e.getType().ordinal() + ',' + e.getX() + ',' + e.getY() + ',' + e.getOption() + ',' + e.getSide());
 	}
 
 	/**
