eae6f34bbe7d598a05353c942a099de9707554cf
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2017-11-29 12:23:52 -0500

    fixed the roll boss so you can't get hit on corners

--- src/mainGame/Player.java
+++ src/mainGame/Player.java
@@ -138,7 +138,7 @@ public class Player extends GameObject {
 					|| tempObject.getId() == ID.RollBoss2 || tempObject.getId() == ID.BullBoss || tempObject.getId() == ID.SeparateBoss 
 					|| tempObject.getId() == ID.SeparateBoss2 || tempObject.getId() == ID.SeparateBoss3) {
 				//player invulnerability
-				//if (tempObject)
+				
 			}
 
 		}
